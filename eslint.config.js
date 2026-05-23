76fdffd3f388a2f0849c4864068aaaeda6c61cc5
Guang Hui <phamquanghuy2809@gmail.com>
2026-05-23 21:25:38 +0700

    test husky, lint-staged

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import { defineConfig } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-import path from 'path'
-import { fileURLToPath } from 'url'
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig([
   {
@@ -54,4 +54,4 @@ export default defineConfig([
       ],
     },
   },
-])
+]);
